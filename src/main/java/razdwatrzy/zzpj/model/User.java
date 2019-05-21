bc45e72b390ccf92cb798a2fa71673ef7513112a
MaciejSocha <maciek.socha1@gmail.com>
2019-05-21 20:53:24 +0200

    Added some columns to Users

--- src/main/java/razdwatrzy/zzpj/model/User.java
+++ src/main/java/razdwatrzy/zzpj/model/User.java
@@ -1,16 +1,29 @@
 package razdwatrzy.zzpj.model;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long user_id;
+    private long id;
 
     @Column
     private String login;
+
+    @Column
+    private Date registrationTime;
+
+    @Column
+    private Boolean isActiveted;
+
+    @Column
+    private Boolean isBanned;
+
+    @Column
+    private Date lastLogin;
 
 }
